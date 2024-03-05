b03846fa528c5f62045d44365bd4ae47d0ed5bc5
elmambou <esmambou@gmail.com>
2024-03-05 19:52:05 +0200

    all additions to final team project

---
+++ swagger.js
@@ -0,0 +1,38 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contact API'
+  },
+  host: 'localhost:8083',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'users',
+      description: 'Operations related to users'
+    }
+  ],
+  definitions: {
+    User: {
+      type: 'object',
+      properties: {
+        username: { type: 'string' },
+        email: { type: 'string' },
+        password: { type: 'string' },
+        role: { type: 'string' },
+        createdAt: { type: 'Date' },
+        updatedAt: { type: 'Date' }
+
+      },
+      required: ['username', 'email', 'password', 'role', 'createdAt', 'updateAt']
+    }
+  }
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
